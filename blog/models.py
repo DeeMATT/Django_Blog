11c6d54d59b5b708871d9c96387caa27c766f518
DeeMATT <mattode@outlook.com>
2019-10-04 02:18:45 +0100

    Added Tagging to blog posts

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -45,6 +46,8 @@ class Post(models.Model):
                              self.publish.day,
                              self.slug])
 
+    tags = TaggableManager()
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post,
@@ -62,3 +65,5 @@ class Comment(models.Model):
     
     def __str__(self):
         return f"Comment by {self.name} on {self.post}"
+
+
