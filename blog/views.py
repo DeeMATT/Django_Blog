7286cc80fdee9201b6f902d7e762d3b186ff09a1
DeeMATT <mattode@outlook.com>
2019-10-05 01:46:14 +0100

    Added full-text search to the blog

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,10 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from config import settings
 
 def post_list(request, tag_slug=None):
@@ -107,22 +108,23 @@ def post_share(request, post_id):
                                                     'sent': sent})
 
 
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
 
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.objects.annotate(
+                        similarity=TrigramSimilarity('title', query),
+                    ).filter(similarity__gt=0.3).order_by('-similarity')
 
-
-
-
-
-
-
-
-
-
-print(
-    'C0deM@sk'
-    )
-
-            
-
-
-            # CodeMonsters Inc.
+    return render(request,
+                'blog/post/search.html',
+                {
+                'form': form,
+                'query': query,
+                'results': results
+                })
